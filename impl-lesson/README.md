7e201f43eb15d2e5207fc9015e68893a2fb544ba
Diego Parra <dp@linkfire.com>
2022-12-16 10:27:50 -0400

    feat: add impl lesson

---
+++ impl-lesson/README.md
@@ -0,0 +1,28 @@
+# Instructions
+Here we're going to workout our knowledge about the `impl`. Follow the instructions to build and exercise your knowledge on the subject.
+
+
+## Do it:
+  - Create a struct `Dimension` containing:
+    - width
+    - height
+    - depth
+
+  - Create a enum `BoxColor` containing:
+    - Color Grey
+    - Color Brown
+
+  - Create a struct for a `ShippingBox` containing:
+    - weight
+    - Dimension 
+    - BoxColor
+
+  - Create a `implementation` for `Dimension` that has the following functions:
+    - print_box_dimensions
+
+  - Create a `implementation` for `BoxColor` that has the following functions:
+    - print_box_color
+
+  - Create a `implementation` for `ShippingBox` that has the following functions:
+    - Create a new Box
+    - Print box characteristics
---
+++ impl-lesson/src/main.rs
@@ -0,0 +1,59 @@
+struct Dimension {
+    width: f64,
+    height: f64,
+    depth: f64,
+}
+
+enum BoxColor {
+    GREY,
+    BROWN,
+}
+
+struct ShippingBox {
+    weight: f64,
+    dimension: Dimension,
+    color: BoxColor,
+}
+
+impl Dimension {
+    fn print_box_dimensions(&self) {
+        println!("width: {}", self.width);
+        println!("height: {}", self.height);
+        println!("depth: {}", self.depth);
+    }
+}
+
+impl BoxColor {
+    fn print_box_color(&self) {
+        match self {
+            BoxColor::GREY => println!("Box Color: Grey"),
+            BoxColor::BROWN => println!("Box Color: Brown"),
+        }
+    }
+}
+impl ShippingBox {
+    fn create_box(weight: f64, dimension: Dimension, color: BoxColor) -> Self {
+        Self {
+            weight: weight,
+            dimension: dimension,
+            color: color,
+        }
+    }
+    fn print_box_info(&self) {
+        self.dimension.print_box_dimensions();
+        self.color.print_box_color();
+        println!("Box weight: {}", self.weight);
+    }
+}
+
+fn main() {
+    let small_box = Dimension {
+        width: 10.0,
+        height: 10.0,
+        depth: 10.0,
+    };
+
+    let shipping_one = ShippingBox::create_box(20.0, small_box, BoxColor::GREY);
+    shipping_one.print_box_info();
+}
+
