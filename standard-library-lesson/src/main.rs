60c9f9fc5d72816943d0c8296df0775c2f0a922a
Diego Parra <dp@linkfire.com>
2022-12-27 15:58:53 -0400

    feat: add standar library lesson

---
+++ standard-library-lesson/src/main.rs
@@ -0,0 +1,7 @@
+fn main() {
+    let string_upper = "DIEGO".to_owned();
+    let string_lower = "parra".to_owned();
+
+    println!("Upper: {:?}", string_lower.to_uppercase());
+    println!("Lower: {:?}", string_upper.to_lowercase());
+}
