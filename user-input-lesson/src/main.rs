f9f6e24f17d166caa5797e1fe108d1e56a759572
Diego Parra <diegoparraferreira@gmail.com>
2023-01-05 14:43:57 -0400

    feat: resolution

--- user-input-lesson/src/main.rs
+++ user-input-lesson/src/main.rs
@@ -1,15 +1,26 @@
 use std::io;
-
 fn get_input() -> io::Result<String> {
     let mut buffer = String::new();
-    
     io::stdin().read_line(&mut buffer)?;
-
     Ok(buffer.trim().to_owned())
 }
 
-
 fn main() {
-    println!("Hello, world!");
+    let mut all_inputs = vec![];
+    let mut counter = 0;
+    while counter < 2 {
+        match get_input() {
+            Ok(word) => { 
+                all_inputs.push(word);
+                counter += 1;
+            }
+            Err(e) => println!("{:?}", e),
+        }
+    }
+
+    for word in  all_inputs {
+        println!("Original: {:?}", word);
+        println!("Transformed to uppercase: {:?}", word.to_uppercase());
+    }
 
 }
